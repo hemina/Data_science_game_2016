b11a2ef848c0f82775931a30d055fa163164b8d1
hemina <mina.he@telecom-bretagne.eu>
2016-06-19 17:47:15 +0200

    correction

--- read_data.py
+++ read_data.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from skimage import io
+import numpy as np
 import os
 import re
 
@@ -7,23 +8,27 @@ rootdir0="/home/mina/data_science_game/roof_images/"
 
 count=0
 dic={}
-data=[]
 #print(os.path.dirname(os.path.abspath(__file__)))
 
 for root, dirnames, filenames in os.walk( rootdir0 ):
     for filename in filenames:
 	count=count+1
 
+	# By using skimage.io.imread, we load the image into numpy.ndarray format.
 	myfile=open(os.path.join(root,filename))
 	img=io.imread(myfile)
+#	print(img.shape) # From the shape we can see that each image is a RGB color image, however the size is different from one another. 
 
+	# We keep only the id for each picture, so that we remove '.jpg' in the filename.
 	filename=re.sub(r'.jpg','',filename)
-
-	dic={'id':filename,'data':img}
-	data.append(dic)
-	if count%100==0:
-		print(dic)
+	
+	dic[filename]=img
+#	data.append(dic)
+	if count%5000==0:
+		print(len(dic))
 print(count)
-ordered=sorted(data)
+ordered=sorted(dic)
 img_data = pd.DataFrame(ordered)
 img_data.to_csv("img_data.csv", sep=';', index=False)
+
+
